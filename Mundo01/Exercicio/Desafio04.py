1ab4cda07cc32bfdf629e7686e56a4ce5e6f959b
Bruno Eduardo De Carvalho Lucena <LUCENAB@essilorluxottica.id>
2026-06-18 22:50:19 -0400

    feat: Desafio 03 - somar 2 números

---
+++ Mundo01/Exercicio/Desafio04.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele.
+alguma_coisa = input("Digite algo: ")
+print(alguma_coisa.isnumeric())
+print(alguma_coisa.isalpha())
+print(alguma_coisa.isdecimal())
+print(alguma_coisa.isupper())
---
+++ Mundo01/aula006.py
@@ -0,0 +1,16 @@
+numero_float = float(input("Digite um numero real"))
+numero_inteiro = int(input("Digite um numero inteiro"))
+string = str(input("Digite uma string"))
+treuorfalse = bool
+
+print("Número real: {} , número inteiro: {}".format(numero_float,numero_inteiro))
+print("String: {}".format(string))
+print("treuorfalse: {}".format(treuorfalse))
+
+print("Somando dois números")
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+s = n1+n2
+
+print(f"A Soma entre {n1} e {n2} vale {s}")
+print("A soma entre {} e {} vale {}".format(n1,n2,s))
